c64b2574bb41c3347eb6cb01aff5470e312a59ea
jude123412 <104434396+jude123412@users.noreply.github.com>
2025-09-15 18:48:20 +0100

    Fix missing research requirement for ammo-loader-tech-artillery

--- prototypes/ammo-loader-compat.lua
+++ prototypes/ammo-loader-compat.lua
@@ -7,4 +7,8 @@ if mods["ammo-loader"] then
             bobmods.lib.tech.add_prerequisite("ammo-loader-tech-loader-chest", "bob-electronics")
         end
     end
+
+    if data.raw.technology["ammo-loader-tech-artillery"] then
+        bobmods.lib.tech.add_prerequisite("ammo-loader-tech-artillery", "utility-science-pack")
+    end
 end
