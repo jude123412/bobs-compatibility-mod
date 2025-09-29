b784e20e4b2b4eadd6020bb1a6c11c0608aa293f
jude123412 <104434396+jude123412@users.noreply.github.com>
2025-09-29 08:36:43 +0100

    Update 2.0.7
    Date: 29. 09. 2025
      Changes:
        - Change Mox Fuel Recipe Icon
        - Change Mox Fuel Research Icon
      Balances:
        - Balance Breeder Fuel Cell Plutonium amount to 3 instead of 1
        - Balance Breeder Fuel Cell Uranium amount to 17 instead of 19
        - Balance Uranium Reprocessing Plutonium output probability to 10% instead of 1%
      Bugfixes:
        - fix missing research prerequisite for Mox Fuel Cells
        - fix missing research prerequisite for Breeder Fuel Cells
        - fix wrong colour for Breeder Fuel research and recipe icon

--- prototypes/nuclear-fuel-compat.lua
+++ prototypes/nuclear-fuel-compat.lua
@@ -43,9 +43,17 @@ if mods["Nuclear Fuel"] then
 
 
     if data.raw.technology["plutonium-breeding"] and mods["reskins-bobs"] and mods["bobplates"] then
+        bobmods.lib.tech.add_prerequisite("plutonium-breeding", "production-science-pack")
         data.raw.technology["plutonium-breeding"].icon = "__bobcompat__/graphics/nuclear-fuel/technology/breeder-fuel-reprocessing-technology.png"
     end
 
+
+    if data.raw.technology["mox-fuel"] then 
+        bobmods.lib.tech.add_prerequisite("mox-fuel", "production-science-pack")
+        if mods["reskins-bobs"] then
+            data.raw.technology["mox-fuel"].icon = "__bobcompat__/graphics/nuclear-fuel/technology/mox-fuel-cell-technology.png"
+        end
+    end
 
     -- Revert to 10 Cells instead of 15
     if data.raw.recipe["nuclear-fuel-reprocessing"] then
@@ -53,10 +61,10 @@ if mods["Nuclear Fuel"] then
             bobmods.lib.recipe.remove_ingredient("nuclear-fuel-reprocessing", "depleted-uranium-fuel-cell")
             bobmods.lib.recipe.add_ingredient("nuclear-fuel-reprocessing", {type = "item", name = "depleted-uranium-fuel-cell", amount = 10})
         end
-        -- Force 1% change for balance with Bobs Plates
+        -- Force 10% change for balance with Bobs Plates
         if mods["bobplates"] then
             bobmods.lib.recipe.remove_result("nuclear-fuel-reprocessing", "plutonium")
-            bobmods.lib.recipe.add_result("nuclear-fuel-reprocessing", {type = "item", name = "plutonium", amount = 1, probability = 0.01})
+            bobmods.lib.recipe.add_result("nuclear-fuel-reprocessing", {type = "item", name = "plutonium", amount = 1, probability = 0.10})
         end
     end
 
@@ -70,7 +78,9 @@ if mods["Nuclear Fuel"] then
 
     if data.raw.recipe["breeder-fuel-cell"] then
         bobmods.lib.recipe.remove_ingredient("breeder-fuel-cell", "uranium-238")
-        bobmods.lib.recipe.add_ingredient("breeder-fuel-cell", {type = "item", name = "uranium-238", amount = 19})
+        bobmods.lib.recipe.remove_ingredient("breeder-fuel-cell", "plutonium")
+        bobmods.lib.recipe.add_ingredient("breeder-fuel-cell", {type = "item", name = "uranium-238", amount = 17})
+        bobmods.lib.recipe.add_ingredient("breeder-fuel-cell", {type = "item", name = "plutonium", amount = 3})
         if data.raw.item["depleted-uranium-fuel-cell"] then
             bobmods.lib.recipe.replace_ingredient("breeder-fuel-cell", "iron-plate", "bob-empty-nuclear-fuel-cell")
         end
